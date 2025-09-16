21e6de63cc3e9521734765fb106fb9a9de7f5e12
Dave <121976848+IamDaveee@users.noreply.github.com>
2025-09-16 14:26:04 +0200

    Add files via upload

---
+++ 20250916/6_Nyelviskola/tablak.sql
@@ -0,0 +1,26 @@
+CREATE TABLE nyelvek (
+  id int(11),
+  nyelv varchar(7) NOT NULL,
+  CONSTRAINT pk_nyelvek PRIMARY KEY (id)
+);
+
+CREATE TABLE vizsgak (
+  sorsz int(11),
+  idopont datetime NOT NULL,
+  nyelvid int(11) NOT NULL,
+  szint varchar(2) NOT NULL,
+  CONSTRAINT pk_vizsgak PRIMARY KEY (sorsz),
+  CONSTRAINT fk_nyelvekvizsgak FOREIGN KEY (nyelvid) REFERENCES nyelvek(id)
+);
+
+CREATE TABLE jelentkezesek (
+  sorsz int(11),
+  nev varchar(18) NOT NULL,
+  mobil varchar(8) NOT NULL,
+  szulev int nOT NULL,
+  vizsga int NOT NULL,
+  CONSTRAINT pk_jelentkezesek PRIMARY KEY (sorsz),
+  CONSTRAINT fk_jelentkezesekvizsga FOREIGN KEY (vizsga) REFERENCES vizsgak(sorsz)
+);
+
+
